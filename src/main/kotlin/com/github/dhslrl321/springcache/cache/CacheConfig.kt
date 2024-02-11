3732ef6d9a12b53bfde84b9e1543a417450310d5
jangwonik <dhslrl321@gmail.com>
2024-02-11 13:51:07 +0900

    commit

--- src/main/kotlin/com/github/dhslrl321/springcache/cache/CacheConfig.kt
+++ src/main/kotlin/com/github/dhslrl321/springcache/cache/CacheConfig.kt
@@ -1,16 +1,27 @@
 package com.github.dhslrl321.springcache.cache
 
-import org.springframework.boot.autoconfigure.cache.CacheProperties.Caffeine
+import com.github.benmanes.caffeine.cache.Caffeine
 import org.springframework.cache.CacheManager
-import org.springframework.cache.caffeine.CaffeineCache
 import org.springframework.cache.caffeine.CaffeineCacheManager
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import java.time.Duration
+import java.time.temporal.ChronoUnit
 
 @Configuration
 class CacheConfig {
     @Bean
-    fun caffeineCacheManager(): CacheManager {
-        return CaffeineCacheManager()
+    fun caffeineCacheManager(caffeine: Caffeine<Any, Any>): CacheManager {
+        val manager = CaffeineCacheManager()
+        manager.setCaffeine(caffeine)
+        return manager
+    }
+
+    @Bean
+    fun caffeine(): Caffeine<Any, Any>? {
+        return Caffeine.newBuilder()
+            .initialCapacity(10)
+            .expireAfterWrite(Duration.of(10, ChronoUnit.SECONDS))
+            .recordStats()
     }
 }
