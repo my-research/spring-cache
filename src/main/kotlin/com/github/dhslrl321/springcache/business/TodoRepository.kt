ac52573c964c9bc3a96ca0ef63d1e861ebef31c3
jangwonik <dhslrl321@gmail.com>
2024-02-18 22:32:38 +0900

    commit

--- src/main/kotlin/com/github/dhslrl321/springcache/business/TodoRepository.kt
+++ src/main/kotlin/com/github/dhslrl321/springcache/business/TodoRepository.kt
@@ -1,7 +1,9 @@
 package com.github.dhslrl321.springcache.business
 
+import org.springframework.data.jpa.repository.Query
 import org.springframework.data.repository.CrudRepository
 
 interface TodoRepository: CrudRepository<Todo, Long> {
+
     fun findAllByUserId(userId: Long): List<Todo>
 }
